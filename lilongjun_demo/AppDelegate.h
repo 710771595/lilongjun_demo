8563fd157bf1907ae65b2722c8f050d9f1484f93
710771595 <lilongjun@192.168.2.127>
2016-11-08 10:18:31 +0800

    Initial Commit

---
+++ lilongjun_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lilongjun_demo
+//
+//  Created by lilongjun on 2016/11/8.
+//  Copyright © 2016年 lilongjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
